a3bddc60d9d91de1ee3659fb3636327122884b79
fatmagulyildirimce@gmail.com <fatmagulyildirimce@gmail.com>
2021-03-13 22:39:52 +0300

    güncellendi

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ namespace Dictionary
         {
             MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
             dictionary.Add(100, "armut");
+            dictionary.Add(150, "elma");
+            dictionary.Add(200, "kivi");
+            dictionary.Add(450, "muz");
+            dictionary.Add(160, "mandalina");
             dictionary.List();
            
         }
